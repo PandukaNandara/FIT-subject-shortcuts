4fbc76b86f79cc1e3458b23a40708fe4d45b8e1c
Panduka Nandara <pandukanandara@outlook.com>
2022-08-02 08:36:40 +0530

    Added new subjects

--- lib/config/it_modules.dart
+++ lib/config/it_modules.dart
@@ -1,6 +1,26 @@
 import 'package:fit_shortcuts/models/models.dart';
 
 const kItModules = ConfigModule(const Level(const [
+  Subject("IN4910", SubjectMode.C, "Comprehensive Group Project", "https://online.uom.lk/course/view.php?id=18542"),
+  Subject("IS3230", SubjectMode.C, "Social Aspects of IT", "https://online.uom.lk/course/view.php?id=18543"),
+  Subject("IS3440", SubjectMode.C, "IT Quality Assurance", "https://online.uom.lk/course/view.php?id=18545"),
+  Subject("CM4340", SubjectMode.E, "Natural Language Processing", "https://online.uom.lk/course/view.php?id=18526"),
+  Subject("IN4620", SubjectMode.E, "Digital Image Processing", "https://online.uom.lk/course/view.php?id=18535"),
+  Subject("IN4700", SubjectMode.E, "Cluster Computing", "https://online.uom.lk/course/view.php?id=18536"),
+  Subject("IN4710", SubjectMode.E, "Human Computer Interaction", "https://online.uom.lk/course/view.php?id=18538"),
+  Subject("IN4300", SubjectMode.E, "Embedded Systems", "https://online.uom.lk/course/view.php?id=18532"),
+  Subject("IN4400", SubjectMode.E, "Data Mining & Data Warehousing", "https://online.uom.lk/course/view.php?id=18533"),
+  Subject("IN4720", SubjectMode.E, "Geographic Information Systems", "https://online.uom.lk/course/view.php?id=18540"),
+  Subject("CM4140", SubjectMode.E, "Operational Research", "https://online.uom.lk/course/view.php?id=18518"),
+  Subject("CM4310", SubjectMode.E, "Artificial Neural Networks & Evolutionary Computing", "https://online.uom.lk/course/view.php?id=18520"),
+  Subject("CM4320", SubjectMode.E, "Complex Systems & Agent Technology", "https://online.uom.lk/course/view.php?id=18522"),
+  Subject("CM4330", SubjectMode.E, "Semantic Web and Ontological Modelling", "https://online.uom.lk/course/view.php?id=18524"),
+  Subject("CM4360", SubjectMode.E, "Robotics", "https://online.uom.lk/course/view.php?id=18528"),
+  Subject("CM4370", SubjectMode.E, "Machine Learning and Pattern Recognition", "https://online.uom.lk/course/view.php?id=18530"),
+  Subject("IS4300", SubjectMode.E, "Marketing Management", "https://online.uom.lk/course/view.php?id=18547"),
+  Subject("IS4330", SubjectMode.E, "Supply Chain Management", "https://online.uom.lk/course/view.php?id=18549"),
+  Subject("IS4340", SubjectMode.E, "Innovation Management", "https://online.uom.lk/course/view.php?id=18516"),
+  Subject("IS4400", SubjectMode.E, "Management of Technology", "https://online.uom.lk/course/view.php?id=18551"),
   Subject(
     "CM 3320",
     SubjectMode.C,
